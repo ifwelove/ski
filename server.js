f18c198a87dd10fe73296b3e774c6923e2274735
ifwelove <a5963745@gmail.com>
2025-03-22 02:10:22 +0800

    更新服務器配置以修復部署問題

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 5000;
 // 靜態文件服務
 app.use(express.static(path.join(__dirname, 'build')));
 
-// 所有請求都導向index.html
+// 所有請求都導向 index.html (對於 React 單頁應用很重要)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`服務運行在端口 ${PORT}`);
+  console.log(`服務已在端口 ${PORT} 啟動`);
 });
